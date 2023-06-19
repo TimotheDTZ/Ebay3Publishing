8c29cd0055d1a84a55dd8080d63890249f153a42
Maxvoltaire <82141695+Maxvoltaire@users.noreply.github.com>
2023-06-20 00:59:53 +0200

    Create AlertIcon architecture.

---
+++ src/stories/ebay1/AlertIcon.tsx
@@ -0,0 +1,13 @@
+// AlertIcon.tsx
+import React from 'react';
+import './AlertIcon.css';
+
+export const AlertIcon = () => {
+    return (
+        <div className="alert-icon">
+            {<img src='test' />}
+        </div>
+    );
+};
+
+export default AlertIcon;
