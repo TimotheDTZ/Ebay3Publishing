577611d5c29f0e0863ec9e957a6b19c3b25a1395
TIDES <tides@mail.com>
2023-06-21 12:51:59 +0200

    toast

--- src/stories/ebay1/Toast.tsx
+++ src/stories/ebay1/Toast.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import './Toast.css';
+import greenCheck from 'remixicon/icons/System/checkbox-circle-line.svg';
+import redDanger from 'remixicon/icons/System/spam-2-line.svg';
+import blueInfo from 'remixicon/icons/System/information-line.svg';
+import yellowWarn from 'remixicon/icons/System/alert-line.svg';
+import close from 'remixicon/icons/System/close-line.svg';
+import Image from "next/image";
 
 export interface ToastProps {
     toastStyle: 'Danger' | 'Warning' | 'Success' | 'Information';
@@ -11,22 +17,40 @@ export const Toast = ({
     input = 'This is a toast message',
     ...props
 }: ToastProps) => {
-    const getToastClass = () => {
-        switch (toastStyle) {
-            case 'Danger':
-                return 'toast toast-danger';
-            case 'Warning':
-                return 'toast toast-warning';
-            case 'Success':
-                return 'toast toast-success';
-            case 'Information':
-                return 'toast toast-information';
-            default:
-                return 'toast';
-        }
-    };
+    let toastLabel = '';
+    let toastImg;
 
-    return <div className={getToastClass()}>{input}</div>;
+    switch (toastStyle) {
+        case 'Danger':
+            toastLabel = 'red';
+            toastImg = redDanger;
+            break;
+        case 'Warning':
+            toastLabel = 'orange';
+            toastImg = yellowWarn;
+            break;
+        case 'Success':
+            toastLabel = 'green';
+            toastImg = greenCheck;
+            break;
+        case 'Information':
+            toastLabel = 'blue';
+            toastImg = blueInfo;
+            break;
+        default:
+            toastLabel = 'toast';
+            break;
+    }
+
+    return <div className={`rebirth-toast ${toastLabel}`}>
+        <div className={'rebirth-toast-img-container'}>
+            <Image src={toastImg} alt={''} className={`rebirth-toast-img`} />
+        </div>
+        <div className={'rebirth-toast-label-container'}>
+            <span className={'toast-label'}>{input}</span>
+            <Image src={close} alt={'close'} className={'close-button'} />
+        </div>
+    </div>;
 };
 
 export default Toast;
